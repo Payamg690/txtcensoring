c301f23872c201267838231b3b66914c3f281521
Payam_G <goodarzi.payam@yahoo.com>
2017-04-22 19:09:00 +0200

    Email Validation fixed

--- secureMail/forms.py
+++ secureMail/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from ckeditor.widgets import CKEditorWidget
 from ckeditor.fields import RichTextField
 from .functions import generatePassword
+from django.core.validators import validate_email
 
 class NachrichtForm(forms.Form):
     Betreff = forms.CharField(widget=forms.TextInput( attrs={'size':'60'}), label='Betreff', max_length=100)
     Nachricht = forms.CharField(widget=CKEditorWidget(config_name='awesome_ckeditor'))
     Email = forms.EmailField(widget=forms.TextInput( attrs={'size':'60'}), label='Email', max_length=254)
+
     
 class PassForm(forms.Form):
     Passwort = forms.CharField(widget=forms.TextInput( attrs={'size':'60'}), label='Passwort', max_length=255)
